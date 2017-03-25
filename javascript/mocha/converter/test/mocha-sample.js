f67f5c8a8419e224fec32db223d755ee966a2abb
mesh1neko <towaduki+github@gmail.com>
2017-03-25 13:01:22 +0900

    til javascript/mocha basic usage

---
+++ javascript/mocha/converter/test/mocha-sample.js
@@ -0,0 +1,8 @@
+const expect = require('chai').expect;
+
+describe('moche sample test', function () {
+  it('sample testcase 1', function () {
+    var result = 1 + 1;
+    expect(2).to.equal(result);
+  });
+});
